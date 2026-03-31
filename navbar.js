d9bd4f52bbdb0f8b4e6d1e6fda9d8ebd132add27
Codering-Monkey <gamer_monkey@outlook.com.au>
2026-03-31 13:26:04 +1100

    ONE OF THE GREATEST NAVBARS EVER

--- navbar.js
+++ navbar.js
@@ -1,49 +1,23 @@
 import { id } from "./script.js"
 
-console.log("navbar.js")
-
 let navbar = id("navbar");
+navbar.classList.add("navbar");
 
-const pages = {
-    "home": ["Home", "index.html"],
-    "person": ["Characters", "characters.html"]
+const pages = {"home": ["Home", "index.html"], "data_thresholding":["Data", "#"], "groups":["Characters", "#"], "map":["Campaigns", "#"], "school":["Learn", "#"], "rewarded_ads":["Awards", "#"]};
+
+function createIcon(icon_name) {
+    let icon = document.createElement("span")
+    icon.classList.add("material-symbols-outlined")
+    icon.textContent = icon_name
+    return icon
 }
 
-navbar.classList.add("navbar")
-
-function createNavElement(icon, name) {
-    let container = document.createElement("div");
-    navbar.appendChild(container);
-
-    let symbol = document.createElement("span");
-    symbol.classList.add("material-symbols-outlined");
-    symbol.textContent = icon;
-    container.appendChild(symbol);
-
-    let message = document.createElement("p");
-    message.textContent = name;
-    container.appendChild(message);
-
-    return container
-}
-
-let menu = createNavElement("menu", "Close")
-menu.addEventListener("click", function () {
-    if (menu.firstChild.textContent === "menu") {
-        menu.firstChild.textContent = "close"
-        navbar.style.width = "20rem";
-    } else {
-        menu.firstChild.textContent = "menu"
-        navbar.style.width = "3rem";
-    }
-})
-
-createNavElement("settings", "Settings").addEventListener("click",
-        function () {window.location.href = "settings.html"})
-
-navbar.spacer()
-
-Object.entries(pages).forEach(([icon, name]) => {
-    createNavElement(icon, name[0]).addEventListener("click",
-        function () {window.location.href = name[1]})
+Object.entries(pages).forEach(([icon, [name, url]]) => {
+    let box = document.createElement("div");
+    navbar.appendChild(box);
+    box.appendChild(createIcon(icon))
+    let littleBox = document.createElement("div");
+    box.appendChild(littleBox)
+    littleBox.textContent = name
+    littleBox.style.left = "-" + littleBox.clientWidth + "px"
 })
