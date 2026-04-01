6bdbb9ae5f0d16d5668ecab23109e6b6600cbd9e
Codering-Monkey <gamer_monkey@outlook.com.au>
2026-04-02 08:58:04 +1100

    imported popup from the lost compendium

--- script.js
+++ script.js
@@ -54,3 +54,68 @@ Object.prototype.invert = function() {
     })
     return newObject
 }
+
+export function popup(data) {
+    const existing = document.getElementsByClassName("popup")
+    const current = existing.length
+    let pop = document.createElement("div")
+    pop.classList.add("popup")
+    pop.id = "popup" + current
+    const destination = (current * 216) + "px"
+    let text = document.createElement("span")
+    text.textContent = data
+    pop.appendChild(text)
+    let bar = document.createElement("div")
+    bar.classList.add("progress-bar")
+    pop.appendChild(bar)
+    let progress = document.createElement("div")
+    progress.classList.add("progress")
+    progress.id = "bar" + current
+    bar.appendChild(progress)
+    document.body.appendChild(pop)
+    setTimeout(function(){
+        pop.style.right = destination
+        setTimeout(function(){
+            fillBar(current)
+        }, 1000);
+    }, 1);
+
+    function fillBar(IdNum) {
+        const oldWidth = stripSign(progress.style.width)
+        const multiplier = (200 - oldWidth) / 200
+        progress.style.width = "200px"
+        progress.style.transitionDuration = (5 * multiplier) + "s"
+        setTimeout(function(){
+            if (window.getComputedStyle(document.getElementById("bar" + IdNum)).getPropertyValue('width') === "200px") {
+                endPopup(IdNum)
+            }
+        }, 5050 * multiplier);
+    }
+
+    function endPopup(IdNum) {
+        const existing = document.getElementsByClassName("popup")
+        const current = existing.length
+        for (let i = IdNum + 1; i < current; i++) {
+            let pop = document.getElementById("popup" + i)
+            const old = stripSign(pop.style.right)
+            pop.style.right = (old - 216) + "px"
+        }
+        let pop = document.getElementById("popup" + IdNum)
+        pop.style.right = "-250px"
+        setTimeout(function () {
+            pop.remove()
+        }, 1000)
+    }
+
+    pop.addEventListener("mouseover", function() {
+        progress.style.width = window.getComputedStyle(document.getElementById("bar" + current)).getPropertyValue('width')
+    })
+
+    pop.addEventListener("mouseout", function () {
+        fillBar(current)
+    })
+
+    pop.addEventListener("click", function () {
+        endPopup(current)
+    })
+}
