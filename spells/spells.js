a017732f5b3e1bb1074b53d6d9a849bfb195a68a
Codering-Monkey <gamer_monkey@outlook.com.au>
2026-04-08 17:26:19 +1000

    added data filtering for name, spell item creation, and triggers

---
+++ spells/spells.js
@@ -0,0 +1,40 @@
+import spellData from "./spells.json" with { type: "json" }
+import { id } from "../script.js"
+
+console.log(spellData)
+
+function filter() {
+    const query = id("searchSpell").value.toLowerCase();
+    let filteredData = structuredClone(spellData)
+    Object.entries(filteredData).forEach(([spellName, spellData]) => {
+        if (!spellName.toLowerCase().includes(query)) {
+            delete filteredData[spellName];
+            return
+        }
+    })
+
+    let parent = id("spellParent")
+    parent.clear()
+    Object.entries(filteredData).forEach(([spellName, spellData]) => {
+        let container = document.createElement("div")
+        container.classList.add("spellOption")
+        parent.append(container)
+
+        let level = document.createElement("span")
+        level.textContent = spellData["Level"]
+        container.appendChild(level)
+
+        let name = document.createElement("span")
+        name.textContent = spellData["Name"]
+        container.appendChild(name)
+
+        let school = document.createElement("span")
+        school.textContent = spellData["School"]
+        container.appendChild(school)
+    })
+}
+
+id("searchButton").addEventListener("click", function() {filter()});
+id("searchSpell").addEventListener("keydown", function(event) {if (event.key === "Enter") {filter()}})
+id("searchSpell").addEventListener("blur", function() {filter()});
+filter()
