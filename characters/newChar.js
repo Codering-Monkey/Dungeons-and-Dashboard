7c8ac0cf18f0e81b9142361cb5985985c8fdd5a0
Codering-Monkey <gamer_monkey@outlook.com.au>
2026-05-06 22:22:52 +1000

    added selectBackground()

--- characters/newChar.js
+++ characters/newChar.js
@@ -1,6 +1,7 @@
 import { id } from "../script.js"
 import classes from "./classes.json" with { type: "json" }
 import species from "./species.json" with { type: "json" }
+import backgrounds from "./backgrounds.json" with { type: "json" }
 
 let newCharacter = {}
 let parent = id("parent")
@@ -10,14 +11,20 @@ next.addEventListener("click", function () {
     if (stage === "Class") {
         newCharacter["Class"] = sessionStorage.get("Class")
         selectSpecies()
+    } else if (stage === "Species") {
+        newCharacter["Species"] = sessionStorage.get("Species")
+        selectBackground()
     }
 })
 let back = id("back")
 back.addEventListener("click", function () {
     let stage = sessionStorage.get("createStage")
     if (stage === "Species") {
-        newCharacter["Class"] = sessionStorage.get("Class")
+        newCharacter["Species"] = sessionStorage.get("Species")
         selectClass()
+    } else if (stage === "Background") {
+        newCharacter["Background"] = sessionStorage.get("Background")
+        selectSpecies()
     }
 })
 
@@ -63,6 +70,31 @@ function selectSpecies() {
         })
         parent.appendChild(listItem)
         if (sessionStorage.get("Species") === key) {
+            listItem.classList.add("active")
+        }
+        let listName = document.createElement("h2")
+        listName.textContent = key
+        listItem.appendChild(listName)
+    })
+}
+
+function selectBackground() {
+    parent.clear()
+    parent.className = "listParent"
+    sessionStorage.set("createStage", "Background")
+    if (!sessionStorage.get("Background")) {
+        sessionStorage.set("Background", "parent")
+    }
+    Object.keys(backgrounds).forEach((key) => {
+        let listItem = document.createElement("div")
+        listItem.id = key
+        listItem.addEventListener("click", function () {
+            id(sessionStorage.get("Background")).classList.remove("active")
+            this.classList.add("active")
+            sessionStorage.set("Background", key)
+        })
+        parent.appendChild(listItem)
+        if (sessionStorage.get("Background") === key) {
             listItem.classList.add("active")
         }
         let listName = document.createElement("h2")
