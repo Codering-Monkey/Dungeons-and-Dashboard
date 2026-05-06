154cdc9e0d8e304b07324d181527eadabbcca958
Codering-Monkey <gamer_monkey@outlook.com.au>
2026-05-06 22:09:00 +1000

    selectClass() done

--- characters/newChar.js
+++ characters/newChar.js
@@ -1,5 +1,37 @@
 import { id } from "../script.js"
+import classes from "./classes.json" with { type: "json" }
 
 let newCharacter = {}
+let parent = id("parent")
+let next = id("next")
+next.addEventListener("click", function () {
+    if (parent.classList.contains("classParent")) {
+        newCharacter["Class"] = sessionStorage.get("Class")
+    }
+})
+let back = id("back")
 
-localStorage.set()
+function selectClass() {
+    parent.clear(1)
+    parent.className = "classParent"
+    Object.keys(classes).forEach((key) => {
+        let classItem = document.createElement("div")
+        classItem.id = key
+        classItem.addEventListener("click", function () {
+            id(sessionStorage.get("Class")).classList.remove("active")
+            this.classList.add("active")
+            sessionStorage.set("Class", key)
+        })
+        parent.appendChild(classItem)
+        if (sessionStorage.get("Class") === key) {
+            classItem.classList.add("active")
+        }
+        let className = document.createElement("h2")
+        className.textContent = key
+        classItem.appendChild(className)
+    })
+}
+
+selectClass()
+
+// localStorage.push("Characters", newCharacter)
