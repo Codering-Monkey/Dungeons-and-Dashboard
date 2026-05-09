ec3ffaf68e44d20df391457338bdde6a65873307
Codering-Monkey <gamer_monkey@outlook.com.au>
2026-05-10 09:48:31 +1000

    stats done

--- characters/newChar.js
+++ characters/newChar.js
@@ -1,7 +1,58 @@
-import { id } from "../script.js"
+import {id, popup} from "../script.js"
 import classes from "./classes.json" with { type: "json" }
 import species from "./species.json" with { type: "json" }
 import backgrounds from "./backgrounds.json" with { type: "json" }
+
+let allStats = {
+    "Strength": "Str",
+    "Dexterity": "Dex",
+    "Constitution": "Con",
+    "Intelligence": "Int",
+    "Wisdom": "Wis",
+    "Charisma": "Cha",
+}
+
+HTMLInputElement.prototype.bonusEvent = function () {
+    this.addEventListener("click", function() {
+        let other
+        let increase
+        other = id(String(Math.abs(this.value - 3)) + this.id.slice(1))
+        increase = parseInt(this.value)
+        let bonusItem = id("totalBonus")
+        if (other.checked) {
+            other.checked = false
+            bonusItem.textContent = String(parseInt(bonusItem.textContent) - Math.abs(this.value - 3))
+        }
+        if (this.checked) {
+            bonusItem.textContent = String(parseInt(bonusItem.textContent) + increase)
+        } else {
+            bonusItem.textContent = String(parseInt(bonusItem.textContent) - increase)
+        }
+        let flippedStats = allStats.invert()
+        let availableAbilities = backgrounds[sessionStorage.get("Background")]["Abilities"]
+        let availableBonus = 3 - parseInt(bonusItem.textContent)
+        for (let i = 0; i < availableAbilities.length; i++) {
+            for (let j = 1; j < 3; j++) {
+                id(j + flippedStats[availableAbilities[i]]).disabled = availableBonus < j && (!id(j + flippedStats[availableAbilities[i]]).checked);
+            }
+        }
+        updateTotal(this.id.slice(1))
+    })
+}
+
+function updateTotal(catagory) {
+    let number = parseInt(id("raw" + catagory).value)
+    if (id("1" + catagory)) {
+        if (id("1" + catagory).checked) {
+            number += 1
+        } else if (id("2" + catagory).checked) {
+            number += 2
+        }
+    }
+    let mod = Math.floor((number - 10) / 2)
+    id("total" + catagory).textContent = String(number)
+    id("mod" + catagory).textContent = mod.symbol()
+}
 
 let newCharacter = {}
 let parent = id("parent")
@@ -13,7 +64,9 @@ next.addEventListener("click", function () {
         selectList(species, "Species")
     } else if (stage === "Species") {
         newCharacter["Species"] = sessionStorage.get("Species")
-        selectBackground(backgrounds, "Background")
+        selectList(backgrounds, "Background")
+    } else if (stage === "Background") {
+        selectStats()
     }
 })
 let back = id("back")
@@ -25,6 +78,8 @@ back.addEventListener("click", function () {
     } else if (stage === "Background") {
         newCharacter["Background"] = sessionStorage.get("Background")
         selectList(species, "Species")
+    } else if (stage === "Stats") {
+        selectList(backgrounds, "Background")
     }
 })
 
@@ -51,6 +106,124 @@ function selectList(data, storageKey) {
         listName.textContent = key
         listItem.appendChild(listName)
     })
+}
+
+function selectStats() {
+    parent.clear()
+    parent.className = "statParent"
+    sessionStorage.set("createStage", "Stats")
+
+    let headers = [
+        "Name",
+        "Base",
+        "Point Cost",
+        "+1 Bonus",
+        "+2 Bonus",
+        "Total",
+        "Modifier"
+    ]
+    for (let i = 0; i < headers.length; i++) {
+        let headerItem = document.createElement("div")
+        headerItem.textContent = headers[i]
+        headerItem.classList.add("header")
+        parent.appendChild(headerItem)
+    }
+
+    Object.entries(allStats).forEach(([key, value]) => {
+        let name = document.createElement("div")
+        name.textContent = key
+        parent.appendChild(name)
+
+        let raw = document.createElement("input")
+        raw.type = "number"
+        raw.id = "raw" + key
+        raw.max = "15"
+        raw.value = "8"
+        raw.min = "8"
+        parent.appendChild(raw)
+        raw.buttons()
+
+        let cost = document.createElement("div")
+        cost.textContent = "0"
+        cost.id = "cost" + key
+        parent.appendChild(cost)
+
+        const statsToPoints = {
+            "8": 0,
+            "9": 1,
+            "10": 2,
+            "11": 3,
+            "12": 4,
+            "13": 5,
+            "14": 7,
+            "15": 9,
+        }
+        raw.addEventListener("change", function() {
+            cost.textContent = String(statsToPoints[raw.value])
+            let totalCost = 0
+            Object.keys(allStats).forEach((statKey) => {
+                totalCost += parseInt(id("cost" + statKey).textContent)
+            })
+            if (totalCost > 27) {
+                popup("You cannot have a Point Buy total greater than 27!")
+            }
+            while (totalCost > 27) {
+                totalCost -= statsToPoints[this.value]
+                this.value = String(parseInt(this.value) - 1)
+                totalCost += statsToPoints[this.value]
+            }
+            cost.textContent = String(statsToPoints[raw.value])
+            id("totalPoints").textContent = String(totalCost)
+            updateTotal(key)
+        })
+
+        if (backgrounds[sessionStorage.get("Background")]["Abilities"].includes(value)) {
+            let one  = document.createElement("input")
+            one.type = "checkbox"
+            one.id = "1" + key
+            one.value = "1"
+            one.bonusEvent()
+            parent.appendChild(one)
+            let two  = document.createElement("input")
+            two.type = "checkbox"
+            two.id = "2" + key
+            two.value = "2"
+            two.bonusEvent()
+            parent.appendChild(two)
+        } else {
+            parent.blank(2)
+        }
+
+        let total = document.createElement("div")
+        total.textContent = "0"
+        total.id = "total" + key
+        parent.appendChild(total)
+
+        let mod = document.createElement("div")
+        mod.textContent = "0"
+        mod.id = "mod" + key
+        parent.appendChild(mod)
+
+        updateTotal(key)
+    })
+
+    let totalTitle = document.createElement("div")
+    totalTitle.classList.add("header")
+    totalTitle.textContent = "Total"
+    parent.appendChild(totalTitle)
+    parent.blank()
+    let totalPoints = document.createElement("div")
+    totalPoints.classList.add("header")
+    totalPoints.id = "totalPoints"
+    totalPoints.textContent = "0"
+    parent.appendChild(totalPoints)
+    let totalBonus = document.createElement("div")
+    totalBonus.classList.add("header")
+    totalBonus.id = "totalBonus"
+    totalBonus.textContent = "0"
+    totalBonus.style.gridColumn = "span 2"
+    parent.appendChild(totalBonus)
+    parent.blank(2)
 }
 
 selectList(classes, "Class")
